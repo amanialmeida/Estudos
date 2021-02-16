a2225d6a14372d1a1949a63681ac994c0f8c35c3
Amanda Abreu <amanialmeida@yahoo.com.br>
2021-02-16 12:11:48 -0300

    Add files via upload

---
+++ oper_aritmeticos.py
@@ -0,0 +1,39 @@
+# Operadores atitméticos em Python
+
+'''
++  -->  soma
+- --> subtração
+* --> multiplicação
+/ --> divisão
+** --> exponenciação
+// --> parte inteira
+% --> módulo, resto da divisão
+'''	
+
+a= 5
+b= 4
+print("a= ", a)
+print("b= ", b)
+
+# Soma
+print ("\nA soma de a+b é ", a+b)
+
+# Subtração
+print ("A subtração a-b  é ", a-b)
+
+# Multiplicação
+print ("A multiplicação de a*b  é ", a*b)
+
+#Divisão
+print("A divisão a/b é ", a/b)
+
+#Exponenciação
+print(" a elevado a b é " , a**b)
+
+#/Parte inteira
+print(" A parte inteira da divisão a/b é " , a//b)
+
+#/Resto
+print(" O resto da divisão  a/b é " , a//b)
+
+
